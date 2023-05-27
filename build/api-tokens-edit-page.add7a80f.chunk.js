0813fb44c0b1fb5001d69df90ffc9a62a1d56a1e
Hieu Mula <hieu@mula-x.com>
2023-05-27 14:54:51 +0700

    set up

---
+++ build/api-tokens-edit-page.add7a80f.chunk.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkshopping_cart=self.webpackChunkshopping_cart||[]).push([[92],{28465:(a,t,e)=>{e.r(t),e.d(t,{default:()=>P});var s=e(67294),n=e(95489),i=e.n(n),E=e(87751),_=e(24583);const P=()=>s.createElement(n.CheckPagePermissions,{permissions:E.Z.settings["api-tokens"].read},s.createElement(_.Z,null))}}]);
---
+++ build/transfer-tokens-create-page.988b4bd1.chunk.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkshopping_cart=self.webpackChunkshopping_cart||[]).push([[4816],{39987:(a,t,e)=>{e.r(t),e.d(t,{default:()=>r});var s=e(67294),n=e(95489),P=e.n(n),E=e(87751),_=e(53219);const r=()=>s.createElement(n.CheckPagePermissions,{permissions:E.Z.settings["transfer-tokens"].create},s.createElement(_.Z,null))}}]);
